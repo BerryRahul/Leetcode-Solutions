5581eccf2adfb087d39194cc5613aaf58df372af
BerryRahul <113147217+BerryRahul@users.noreply.github.com>
2024-08-13 19:49:09 -0400

    added documentation

--- Arrays & Hashing /347. Top K Frequent Elements.py
+++ Arrays & Hashing /347. Top K Frequent Elements.py
@@ -4,6 +4,12 @@ def topKFrequent(nums, k):
     :type k: int
     :rtype: List[int]
     """
+    # this solution is O(n) + O(n) = O(n) time and memory 
+    # A variation of bucket sort and heapify 
+    # create an array subarrays with the length of nums 
+    # count the frequency of each element in nums into a hashmap
+    # then append the value into the index of the frequency array which indicates how many times any element oocurs 
+    # then itterate through that array backwards appending to the final result array up until you have k items
     count = {}
     freq = [[] for i in range(len(nums) + 1)]
 
