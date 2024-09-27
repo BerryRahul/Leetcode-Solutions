e1af5672ff4b5ab03892002cad3471be87696152
BerryRahul <113147217+BerryRahul@users.noreply.github.com>
2024-09-27 15:45:25 -0400

    added documentation

--- Arrays & Hashing /2351. First Letter to Appear Twice.py
+++ Arrays & Hashing /2351. First Letter to Appear Twice.py
@@ -3,6 +3,8 @@ def repeatedCharacter(s):
     :type s: str
     :rtype: str
     """
+    # A hashset is not necessary given the initial conditions 
+    # also because we are only keeping track of lowercase letters 
     tracker = []
     for i in s: 
         if i in tracker: 
