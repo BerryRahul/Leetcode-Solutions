b881dfdaa3719a49236874a295b7756495d609b7
BerryRahul <113147217+BerryRahul@users.noreply.github.com>
2024-09-28 16:28:38 -0400

    added documentation

--- Arrays & Hashing /287. Find the Duplicate Number.py
+++ Arrays & Hashing /287. Find the Duplicate Number.py
@@ -3,12 +3,17 @@ def findDuplicate(nums):
         :type nums: List[int]
         :rtype: int
         """
+        # Find the first point of intersection 
         slow, fast = 0, 0 
         while True: 
                 slow = nums[slow]
                 fast = nums[nums[fast]] # this moves twice as fast 
                 if slow == fast: 
                         break 
+        # after first point of intersection leave slow as is 
+        # slow2 will be used to find the second point of intersection 
+        # due to the preconditions of this problem the second 
+        # point of intersection will always be the duplicate (floyds)
         slow2 = 0
         while True: 
             slow = nums[slow]
