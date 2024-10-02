7d82e8a0dc7d35f705c9adf2cc22a291eb9f2d99
BerryRahul <113147217+BerryRahul@users.noreply.github.com>
2024-10-02 09:22:57 -0400

    implemented 771

---
+++ Arrays & Hashing /771. Jewels and Stones.py
@@ -0,0 +1,13 @@
+def numJewelsInStones(jewels, stones):
+    """
+    :type jewels: str
+    :type stones: str
+    :rtype: int
+    """
+    c = 0
+    for i in set(jewels):
+        c += stones.count(i)
+    return c
+
+
+numJewelsInStones("aA", "aAAbbbb")
